8736e8aff59742f3ffdfb15c1cacf735788f7684
Igor Onachenko <developer@predanie.ru>
2015-11-02 23:58:39 +0300

    fix travel routing bug

--- src/AppBundle/Controller/TravelController.php
+++ src/AppBundle/Controller/TravelController.php
@@ -12,15 +12,13 @@ use AppBundle\Form\TravelType;
 
 /**
  * Travel controller.
- *
- * @Route("/travel")
  */
 class TravelController extends Controller
 {
     /**
      * Search Travels
      *
-     * @Route("/search", name="search_travel")
+     * @Route("/travel/search", name="search_travel")
      * @Template()
      * @param Request $request
      * @return array
@@ -36,7 +34,7 @@ class TravelController extends Controller
     /**
      * Finds and displays a Travel
      *
-     * @Route("/{id}", name="travel_show")
+     * @Route("/travel/{id}", name="travel_show")
      * @Template()
      * @param Travel $travel
      * @return array
@@ -44,190 +42,5 @@ class TravelController extends Controller
     public function showAction(Travel $travel)
     {
         return ['travel' => $travel];
-    }
-
-    /**
-     * Creates a new Travel entity.
-     *
-     * @Route("/", name="travel_create")
-     * @Method("POST")
-     * @Template("AppBundle:Travel:new.html.twig")
-     */
-    public function createAction(Request $request)
-    {
-        $entity = new Travel();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('travel_show', ['id' => $entity->getId()]));
-        }
-
-        return [
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ];
-    }
-
-    /**
-     * Creates a form to create a Travel entity.
-     *
-     * @param Travel $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Travel $entity)
-    {
-        $form = $this->createForm(new TravelType(), $entity, array(
-            'action' => $this->generateUrl('travel_create'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Create'));
-
-        return $form;
-    }
-
-    /**
-     * Displays a form to create a new Travel entity.
-     *
-     * @Route("/new", name="travel_new")
-     * @Method("GET")
-     * @Template()
-     */
-    public function newAction()
-    {
-        $entity = new Travel();
-        $form   = $this->createCreateForm($entity);
-
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
-
-    /**
-     * Displays a form to edit an existing Travel entity.
-     *
-     * @Route("/{id}/edit", name="travel_edit")
-     * @Method("GET")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('AppBundle:Travel')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Travel entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-    * Creates a form to edit a Travel entity.
-    *
-    * @param Travel $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Travel $entity)
-    {
-        $form = $this->createForm(new TravelType(), $entity, array(
-            'action' => $this->generateUrl('travel_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-    /**
-     * Edits an existing Travel entity.
-     *
-     * @Route("/{id}", name="travel_update")
-     * @Method("PUT")
-     * @Template("AppBundle:Travel:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('AppBundle:Travel')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Travel entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('travel_edit', array('id' => $id)));
-        }
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-    /**
-     * Deletes a Travel entity.
-     *
-     * @Route("/{id}", name="travel_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('AppBundle:Travel')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Travel entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('travel'));
-    }
-
-    /**
-     * Creates a form to delete a Travel entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('travel_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
     }
 }
